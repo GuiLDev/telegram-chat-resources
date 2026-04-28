a6b8a9cf9185112212d7096e36a510ecb1ba338e
Guilherme <guilhermerebelo.dev@gmail.com>
2026-04-28 10:10:17 -0400

    implementação do envio da mensagem pelo bot

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import { sendMessage } from "./services/telegram.js";
 
 //Essa parte le o .env e configura ele no resto do código
 dotenv.config();
@@ -18,6 +19,21 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Servidor do bot Telegram rodando!");
+});
+
+//Essa parte do código é responsavel por enviar a mensagem para o chat_id pelo BOT
+app.get("/test-message", async (req, res) => {
+  const chatId = process.env.TEST_CHAT_ID;
+
+  if (!chatId) {
+    return res.status(400).send("TEST_CHAT_ID não encontrado no .env");
+  }
+
+  const result = await sendMessage(chatId, "Mensagem de teste enviada pelo bot!");
+
+  console.log(result);
+
+  res.send("Mensagem enviada!");
 });
 
 app.listen(PORT, () => {
