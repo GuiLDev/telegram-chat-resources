bf04f6ef6917502ddcc24bfed5a6345f3564180f
GuilhermeL <guilhermerebelo.dev@gmail.com>
2026-04-27 22:04:14 -0400

    criado condição para rodar um log do token do bot

--- src/index.js
+++ src/index.js
@@ -6,6 +6,13 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const BOT_TOKEN = process.env.BOT_TOKEN;
+
+if (!BOT_TOKEN){
+  console.log('BOT_TOKEN não encontrado no .env');
+}else{
+  console.log('BOT_TOKEN carregado com sucesso')
+}
 
 app.use(express.json());
 
