38073ffb6691891d93a46b8b3130e83193fe9104
GuilhermeL <guilhermerebelo.dev@gmail.com>
2026-04-27 22:02:41 -0400

    metodo POST criado para comunicar com a API do Telegram

---
+++ src/services/telegram.js
@@ -0,0 +1,23 @@
+const BOT_TOKEN = process.env.BOT_TOKEN;
+const TELEGRAM_API_URL = `https://api.telegram.org/bot${BOT_TOKEN}`;
+
+
+//função pode exportar futuramente com export para outros metodos também
+//o async é uma forma de identificar que a função pode demorar responder
+//ele espera a resposta da API e então ativa o metodo POST para enviar uma mensagem
+export async function sendMessage(chatId, text) {
+  const response = await fetch(`${TELEGRAM_API_URL}/sendMessage`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      chat_id: chatId,
+      text,
+    }),
+  });
+
+  const data = await response.json();
+
+  return data;
+}
