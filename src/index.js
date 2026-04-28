9a7390f8a1143a39a878840dee1c115d85b6f6aa
Guilherme <guilhermerebelo.dev@gmail.com>
2026-04-28 17:00:15 -0400

    Novas funcionalidades nos comandos e MENU

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import { sendMessage } from "./services/telegram.js";
+import { sendMessage, setBotCommands } from "./services/telegram.js";
 
 //Essa parte le o .env e configura ele no resto do código
 dotenv.config();
@@ -21,6 +21,42 @@ app.get("/", (req, res) => {
   res.send("Servidor do bot Telegram rodando!");
 });
 
+//Essa parte é onde recebe a requisição do POST e mostra no terminal
+app.post("/webhook", async (req, res) => {
+  const update = req.body;
+
+  console.log("Update recebido:");
+  console.log(update);
+
+  const message = update.message;
+  const text = message?.text;
+  const chatId = message?.chat?.id;
+
+  if (text === "/start") {
+    await sendMessage(chatId, "Olá! Bot iniciado com sucesso.");
+  }
+
+  if (text === "/help") {
+    await sendMessage(chatId, "Comandos disponíveis:\n\n/start - Iniciar o bot\n/help - Ver ajuda\n/menu - Abrir menu principal");
+  }
+
+  if (text === "/menu") {
+  await sendMessage(chatId, "Menu principal:", {
+    reply_markup: {
+      inline_keyboard: [
+        [{ text: "Criar item", callback_data: "create_item" }],
+        [{ text: "Listar itens", callback_data: "list_items" }],
+        [{ text: "Atualizar item", callback_data: "update_item" }],
+        [{ text: "Deletar item", callback_data: "delete_item" }],
+      ],
+    },
+  });
+}
+
+  res.sendStatus(200);
+});
+
+
 //Essa parte do código é responsavel por enviar a mensagem para o chat_id pelo BOT
 app.get("/test-message", async (req, res) => {
   const chatId = process.env.TEST_CHAT_ID;
@@ -34,6 +70,14 @@ app.get("/test-message", async (req, res) => {
   console.log(result);
 
   res.send("Mensagem enviada!");
+});
+
+app.get("/set-commands", async (req, res) => {
+  const result = await setBotCommands();
+
+  console.log(result);
+
+  res.send(result);
 });
 
 app.listen(PORT, () => {
